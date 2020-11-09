554a9f38d400c6bab3245ba0733e8c81fbb192f8
Reza Babaee <mrezababaee@gmail.com>
2020-11-09 13:49:43 +0100

    updated the transform by adding comments

--- transform3D.py
+++ transform3D.py
@@ -2,16 +2,35 @@ import numpy as np
 
 
 class Transform3D:
+    """
+     Rigid transformation of two sets of 3D points using umeyama method
+
+     Inputs:
+       source_points - a n * 3 matrix whose rows are points
+       target_points - a n * 3 matrix whose rows are points that correspond to
+     Outputs:
+
+       c, R, t - the scaling, rotation matrix, and translation vector
+                 defining the linear map TR as
+
+                           TR(x) = c * R * source_points + t
+
+                 such that the average norm of TR(X(i, :) - target_points(i, :))
+                 is minimized.
+    """
 
     def __init__(self, number_of_points):
         self.number_of_points = number_of_points
         self.dimension = 3
 
     def compute_rigid_transform(self, source_points, target_points):
-        # Input: both source_points and target_points are Nx3 matrix of points
-        # Returns R,translation
-        # R = 3x3 rotation matrix
-        # translation = 3x1 column vector
+        """
+        :param source_points: A n * 3 matrix of source points
+        :param target_points:  A n * 3 matrix of target points
+        :return: returns rotation and translation
+                 Rotation is a 3*3 matrix
+                 Translation is a 3*1 vector
+        """
         assert self.number_of_points == source_points.shape[0]  # total points
         assert self.dimension == source_points.shape[1]
         assert len(source_points) == len(target_points)
@@ -25,13 +44,13 @@ class Transform3D:
         H = np.dot(np.transpose(centered_target_points), centered_source_points)/self.number_of_points
         U, D, V = np.linalg.svd(H, full_matrices=True, compute_uv=True)
         V = V.T.copy()
-        r = np.ndim(H)
-        d = np.linalg.det(H)
+        rank_h = np.ndim(H)
+        determinat_h = np.linalg.det(H)
         S = np.eye(self.dimension)
-        if r > self.dimension - 1:
-            if np.linalg.det(H) < 0:
+        if rank_h > self.dimension - 1:
+            if determinat_h < 0:
                 S[self.dimension, self.dimension] = -1
-            elif r == self.dimension - 1:
+            elif rank_h == self.dimension - 1:
                 if np.linalg.det(U) * np.linalg.det(V) < 0:
                     S[self.dimension, self.dimension] = -1
             else:
